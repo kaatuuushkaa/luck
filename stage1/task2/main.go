0e1b7f3a12432882a3615a49afd051049c9a9ebc
Екатерина Торощина <ekaterinatorosina@MacBook-Air-Ekaterina.local>
2025-06-09 20:03:52 -0700

    done

---
+++ stage1/task2/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+var task []string
+
+type TaskRequest struct {
+	ID   int    `json:"id"`
+	Task string `json:"task"`
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		var req TaskRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, fmt.Sprintf("Invalid request body: %v", err), http.StatusBadRequest)
+			return
+		}
+		task = append(task, req.Task)
+	} else {
+		fmt.Fprintln(w, "Only POST")
+	}
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Fprintf(w, "hello, %s", task)
+	} else {
+		fmt.Fprintln(w, "Only GET")
+	}
+}
+
+//1. Реализовать `PATCH` ручку, которая будет обновлять `task` *(Саму задачу или ее статус)* по `ID`
+//2. Реализовать `DELETE` ручку, которая будет удалять `task` по `ID`
+
+func PatchHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPatch {
+		var req TaskRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, fmt.Sprintf("Invalid request body: %v", err), http.StatusBadRequest)
+		}
+		if req.ID <= 0 || req.ID > len(task) {
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
+			return
+		}
+		task[req.ID-1] = req.Task
+	} else {
+		fmt.Fprintln(w, "Only PATCH")
+	}
+}
+
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodDelete {
+		var req struct {
+			ID int `json:"id"`
+		}
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, fmt.Sprintf("Invalid request body: %v", err), http.StatusBadRequest)
+		}
+		if req.ID <= 0 || req.ID > len(task) {
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
+			return
+		}
+		task = append(task[:req.ID-1], task[req.ID:]...)
+	} else {
+		fmt.Fprintln(w, "Only DELETE")
+	}
+}
+
+func main() {
+	http.HandleFunc("/post", PostHandler)
+	http.HandleFunc("/get", GetHandler)
+	http.HandleFunc("/patch", PatchHandler)
+	http.HandleFunc("/delete", DeleteHandler)
+	http.ListenAndServe("localhost:8080", nil)
+}
